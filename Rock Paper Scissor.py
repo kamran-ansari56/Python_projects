d7866e51c0434993f543bdd140f82caf86187088
Kamran Ansari <kamran.ansari56.ka@gmail.com>
2025-09-22 17:20:03 +0530

    mod

--- Rock Paper Scissor.py
+++ Rock Paper Scissor.py
@@ -4,3 +4,6 @@ print("Winning Rules of the game ROCK PAPER SCISSORs are:\n"
 + "ROCK vs PAPER ----> Paper Wins\n"
 + "ROCK vs SCISSOR ----> Rock Wins\n"
 + "PAPER vs SCISSOR ----> Scissor Wins\n")
+
+while True:
+    print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissor \n")
