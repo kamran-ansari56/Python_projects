ff8ecae9a7cab00e151d502a9541a8794adb22a9
Kamran Ansari <kamran.ansari56.ka@gmail.com>
2025-09-22 17:17:54 +0530

    mod

---
+++ Rock Paper Scissor.py
@@ -0,0 +1,6 @@
+import random
+
+print("Winning Rules of the game ROCK PAPER SCISSORs are:\n"
++ "ROCK vs PAPER ----> Paper Wins\n"
++ "ROCK vs SCISSOR ----> Rock Wins\n"
++ "PAPER vs SCISSOR ----> Scissor Wins\n")
